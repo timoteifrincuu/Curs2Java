382f1434488950096842f730eab2e964cee39227
timoteifrincuu <146390788+timoteifrincuu@users.noreply.github.com>
2024-03-09 12:37:13 +0200

    Update Ex10.java

--- Ex10.java
+++ Ex10.java
@@ -2,7 +2,22 @@
 public class Ex10 {
 
 	public static void main(String[] args) {
+		int n = 1001;
 		
+		while(!ePrim(n)){
+			n++;
+		}
+		
+		System.out.println("cel mai mic  nr prim mai mare de 1000 e " + n);
+	}
+	
+		public static boolean ePrim(int n){
+			for(int i = 2; i< n; i++){
+				if(n%i==0){
+					return false;
+				}
+			}
+			return true;
 	}
 
 }
